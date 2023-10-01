824ab7736f0684d6abdaedfa4587a8ba4535ac1f
reowicaksono <reo.wicaksono.2005356@gmail.com>
2023-10-01 19:11:27 +0700

    train unit test and get data with postgresql

--- src/server.js
+++ src/server.js
@@ -1,11 +1,24 @@
-const express = require('express')
+import express from "express";
+import dotenv from "dotenv";
+import { PrismaClient } from "@prisma/client";
+
+
+dotenv.config();
 const app = express()
-const port = 3000
+const PORT = process.env.PORT;
+const prisma = new PrismaClient();
+
+const testGet = app.get('/', (req, res) => {
+  res.status(200).json({ message: 'Hello World!' });
+});
+
+app.get('/test-prisma', async (req, res) => {
+  const getAllUser = await prisma.user.findMany();
+
+  res.send(getAllUser);
+});
 
 
-app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
 app.get('/about/:id', async (req, res) => { 
   const statusCode = res.statusCode = 200
   await res.setHeader('Content-Type', 'text/html')
@@ -22,6 +35,8 @@ app.get('/about/:id', async (req, res) => {
 })
 app.use(express.static('public'))
 
-app.listen(port, '127.0.0.1',() => {
-  console.log(`Example a  pp listening on port ${port}`)
+app.listen(PORT, '127.0.0.1',() => {
+  console.log(`Example app listening on PORT ${PORT}`)
 })
+
+
